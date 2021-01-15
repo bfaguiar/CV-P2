927ac737411330884078c44d65d2921e3d444e5a
Bruno Aguiar <brunoaguiar@Brunos-MBP.netcabo.pt>
2021-01-15 12:52:38 +0000

    fixes

--- damas.py
+++ damas.py
@@ -1,22 +1,11 @@
 #
-#
 import urllib.request
 import cv2 as cv
 import numpy as np
 import time
 import sys
-
-import glob
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
-
+#
+#
 url='http://192.168.1.83:8080/shot.jpg' ## default IP
 
 if len(sys.argv) == 2:
